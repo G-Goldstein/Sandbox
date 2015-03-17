4509eb6765546e33ba2800b32620b293e0e5d0f0
G-Goldstein <g.goldstein@live.com>
2015-03-17 21:39:57 +0000

    Added a tiny Calculator application and test
    In order to ensure that this works, I've added simple code into
    src/main/scala and src/test/scala.

---
+++ src/main/scala/calculator/Calculator.scala
@@ -0,0 +1,9 @@
+package calculator
+
+object Calculator extends App {
+  
+  def sum(a: Int, b: Int) = a + b
+  
+  println(sum(1, 2))
+  
+}
---
+++ src/test/scala/calculator/CalculatorSpec.scala
@@ -0,0 +1,11 @@
+package calculator
+
+import org.scalatest._
+
+class CalculatorSpec extends FlatSpec with Matchers {
+  
+  "Sum" should "correctly sum up two numbers" in {
+    Calculator.sum(3,4) should be (7)
+  }
+  
+}
